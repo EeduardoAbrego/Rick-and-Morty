41c9faa8afc9dfcdb3ea017a8b063a289765c064
eduardoabrego11 <eduardoabrego11@gmail.com>
2023-08-12 18:12:44 -0300

    atualizacion

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,11 +1,42 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useState } from "react";
+import { connect, useDispatch } from "react-redux";
 import Card from "./Card"
+import { filterCards, orderCards } from "../redux/action";
 
 function Favorites({myFavorites}) {
+
+    const [aux , setAux] = useState(false);
+  
+    const dispatch = useDispatch();
+
+    const handleOrder = (e) => {
+         const {value} = e.target;
+         setAux(!aux)
+         dispatch(orderCards(value));
+    };
+
+    const handleFilter = (e) => {
+        const {value} = e.target;
+        setAux(!aux)
+        dispatch(filterCards(value));
+    };
+
+
     return(
         <div>
-            {myFavorites?.map(({name, status, species, image, id})=> (
+              <>
+              <select onChange={handleOrder} >
+              <option value="A">Ascendente</option>
+              <option value="D">Descendente</option>
+              </select>
+              <select onChange={handleFilter} >
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Genderless">Genderles</option>
+              <option value="unknown">unknow</option>
+              </select>
+              </>
+              {myFavorites?.map(({name, status, species, image, id})=> (
                 <>
                 <hr/>
                 <Card
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,19 +1,40 @@
-import { ADD_FAV , REMOVE_FAV } from "./action"
+import { ADD_FAV , REMOVE_FAV , ORDER, FILTER} from "./action"
 
 
  const initialState = {
     myFavorites: [],
+    allCharacters: []
  };
 
  const reducer = (state=initialState,action ) => {
   switch (action.type) {
     case ADD_FAV:
-        return {...state, myFavorites:[...state.myFavorites, action.payload]  };
+        return {...state, allCharacters:[...state.allCharacters, action.payload], myFavorites:[...state.allCharacters, action.payload] };
         
     case REMOVE_FAV:
          return {...state, myFavorites: state.myFavorites.filter((char) => char.id !== Number(action.payload)) 
          };
-  
+
+    case FILTER:
+        const charFilter = [...state.allCharacters].filter((char)=> char.gender === action.payload)    
+          return {
+            ...state,
+            myFavorites:charFilter
+          };
+    
+    case ORDER:
+      const charSort = [...state.allCharacters].sort((a,b)=> {
+        if(action.payload === "A") {
+            return a.id > b.id ? 1 : -1
+        } else {
+            return a.id < b.id ? 1 : -1
+        }
+        });
+        return {
+            ...state,
+            myFavorites:charSort
+        }  
+    
     default:
         return { ...state}
     }
